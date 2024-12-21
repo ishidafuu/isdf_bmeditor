3b39acb32f678e2b5330e3cce692d2901c3bd7db
ishidafuu <ishidafuu@gmail.com>
2024-12-21 14:29:38 +0900

    wip

--- ViewModels/BattleMotionEditorViewModel.cs
+++ ViewModels/BattleMotionEditorViewModel.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using ReactiveUI;
+using System.Reactive;
 using isdf_bmeditor.Models;
 
 namespace isdf_bmeditor.ViewModels;
@@ -9,9 +11,23 @@ namespace isdf_bmeditor.ViewModels;
 /// </summary>
 public class BattleMotionEditorViewModel : ViewModelBase
 {
-    private List<Motion> _motions = new();
+    private ObservableCollection<Motion> _motions = new();
     private int _activeMotionIndex;
     private int _activeKomaIndex;
+
+    public BattleMotionEditorViewModel()
+    {
+        ChangeKomaIndexCommand = ReactiveCommand.Create<int>(ChangeKomaIndex);
+    }
+
+    /// <summary>
+    /// モーションリスト
+    /// </summary>
+    public ObservableCollection<Motion> Motions
+    {
+        get => _motions;
+        set => this.RaiseAndSetIfChanged(ref _motions, value);
+    }
 
     /// <summary>
     /// 現在選択中のモーションインデックス
@@ -34,10 +50,28 @@ public class BattleMotionEditorViewModel : ViewModelBase
     /// <summary>
     /// 現在選択中のモーション情報
     /// </summary>
-    public Motion? ActiveMotion => _motions.Count > _activeMotionIndex ? _motions[_activeMotionIndex] : null;
+    public Motion? ActiveMotion => Motions.Count > ActiveMotionIndex ? Motions[ActiveMotionIndex] : null;
 
     /// <summary>
     /// 現在選択中のコマ情報
     /// </summary>
-    public Koma? ActiveKoma => ActiveMotion?.Komas.Count > _activeKomaIndex ? ActiveMotion.Komas[_activeKomaIndex] : null;
+    public Koma? ActiveKoma => ActiveMotion?.Komas.Count > ActiveKomaIndex ? ActiveMotion.Komas[ActiveKomaIndex] : null;
+
+    /// <summary>
+    /// コマインデックス変更コマンド
+    /// </summary>
+    public ReactiveCommand<int, Unit> ChangeKomaIndexCommand { get; }
+
+    /// <summary>
+    /// コマインデックスを変更する
+    /// </summary>
+    /// <param name="amount">変更量（正の値で増加、負の値で減少）</param>
+    private void ChangeKomaIndex(int amount)
+    {
+        var newIndex = ActiveKomaIndex + amount;
+        if (ActiveMotion != null && newIndex >= 0 && newIndex < ActiveMotion.Komas.Count)
+        {
+            ActiveKomaIndex = newIndex;
+        }
+    }
 } 
