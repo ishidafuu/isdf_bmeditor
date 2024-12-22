67e957d8bc9c198edb0df62361ee38d205b01083
ishidafuu <ishidafuu@gmail.com>
2024-12-22 12:06:21 +0900

    wip

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -10,6 +10,24 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        // DataContextが設定された後に位置を設定
+        DataContextChanged += (sender, e) =>
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+            {
+                Position = new PixelPoint((int)viewModel.WindowPosition.X, (int)viewModel.WindowPosition.Y);
+            }
+        };
+
+        // ウィンドウの位置が変更されたときにViewModelを更新
+        PositionChanged += (sender, e) =>
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+            {
+                viewModel.WindowPosition = new Point(Position.X, Position.Y);
+            }
+        };
     }
 
     private void OnClosing(object? sender, WindowClosingEventArgs e)
